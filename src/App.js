83c7f42a70092856f24b9a6bcc727e2715d9ca8e
asadadi786 <57137231+asadadi786@users.noreply.github.com>
2020-06-14 17:00:13 +0500

    update css App.js

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
       Desi MummyDaddy Restaurant
       </h1>
     <div style={{backgroundColor : "lightblue", color :"white",fontFamily :"arial", fontSize : "10", textAlign : "center"}}> 
-    <h2>
+    
       Menu 1:
     <Dinner dish ="Mutton Chop" sweet =  "Saviyan" tea = "Chaiii"/>
-    </h2>
+    
     <hr/>
     Menu 2:
     <Dinner dish ="Nehari" sweet =  "Custurd" tea = "Chaiii"/>
